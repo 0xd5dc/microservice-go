c3db176827732a67978f449280d9d9b6a760230a
Ergo <summermick@gmail.com>
2020-07-15 21:48:51 -0700

    Repository tests added to Event and User models

--- services/repository_test.go
+++ services/repository_test.go
@@ -2,27 +2,38 @@ package service
 
 import (
 	model "awesomeProject/models"
-	"fmt"
-	"log"
 	"testing"
 )
 
-func TestGetOne(t *testing.T) {
-	user := model.CreateObject(model.User{})
-	// generated a fake user
-	createdUser, err := CreateOne(user, "users")
-	if err != nil {
-		log.Fatal(err)
-	}
-	foundUser, err := GetOne(createdUser, "users")
-	if createdUser != foundUser {
-		fmt.Printf("CreateOne with ID: %+v, but find:%+v\n", createdUser, foundUser)
+func TestCreateOneGetOne(t *testing.T) {
+	t.Run("create_and_find_a user", createTest(model.User{}, "users"))
+	t.Run("create_and_find_a_event", createTest(model.Event{}, "events"))
+}
+
+func TestDeleteOne(t *testing.T) {
+	t.Run("delete_a_user", deleteTest(model.User{}, "users"))
+	t.Run("delete_a_event", deleteTest(model.Event{}, "events"))
+}
+
+func deleteTest(object interface{}, col string) func(t *testing.T) {
+	return func(t *testing.T) {
+		obj := model.CreateObject(object)
+		CreateOne(obj, col)
+		result, _ := DeleteOne(obj, col)
+		if result.DeletedCount < 1 {
+			t.Errorf("DeleteOne removed %+v document(s)\n", result.DeletedCount)
+		}
 	}
 }
-func TestCreateOne(t *testing.T) {
-	user := model.CreateObject(model.User{})
-	_, err := CreateOne(user, "users")
-	if err != nil {
-		log.Fatal(err)
+
+func createTest(object interface{}, col string) func(t *testing.T) {
+	return func(t *testing.T) {
+		obj := model.CreateObject(object)
+		// generated a fake obj
+		createdObject, _ := CreateOne(obj, col)
+		foundObject, _ := GetOne(createdObject, col)
+		if createdObject != foundObject {
+			t.Errorf("CreateOne with ID: %+v, but find:%+v\n", createdObject, foundObject)
+		}
 	}
 }
