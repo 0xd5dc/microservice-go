377786b3457859957c9797304b51c826b990a54e
Ergo <summermick@gmail.com>
2020-07-15 23:27:22 -0700

    DRY repository_test.go

--- services/repository_test.go
+++ services/repository_test.go
@@ -5,14 +5,13 @@ import (
 	"testing"
 )
 
-func TestCreateOneGetOne(t *testing.T) {
-	t.Run("create_and_find_a user", createTest(model.User{}, "users"))
-	t.Run("create_and_find_a_event", createTest(model.Event{}, "events"))
-}
-
-func TestDeleteOne(t *testing.T) {
+func TestRepository(t *testing.T) {
+	t.Run("create_a user", createTest(model.User{}, "users"))
 	t.Run("delete_a_user", deleteTest(model.User{}, "users"))
+	t.Run("create_a_event", createTest(model.Event{}, "events"))
 	t.Run("delete_a_event", deleteTest(model.Event{}, "events"))
+	t.Run("update_a_user", updateTest(model.User{}, "users"))
+	t.Run("update_a_event", updateTest(model.Event{}, "events"))
 }
 
 func deleteTest(object interface{}, col string) func(t *testing.T) {
@@ -30,10 +29,24 @@ func createTest(object interface{}, col string) func(t *testing.T) {
 	return func(t *testing.T) {
 		obj := model.CreateObject(object)
 		// generated a fake obj
-		createdObject, _ := CreateOne(obj, col)
-		foundObject, _ := GetOne(createdObject, col)
-		if createdObject != foundObject {
-			t.Errorf("CreateOne with ID: %+v, but find:%+v\n", createdObject, foundObject)
+		result, _ := CreateOne(obj, col)
+		if result.InsertedID == nil {
+			t.Errorf("CreateOne failed to create!")
+		}
+	}
+}
+func updateTest(object interface{}, col string) func(t *testing.T) {
+	return func(t *testing.T) {
+		obj := model.CreateObject(object)
+		CreateOne(obj, col)
+		newObj := model.CreateObject(object)
+		// generated a fake obj
+		result, _ := UpdateOne(obj, newObj, col)
+		if result.MatchedCount < 1 {
+			t.Errorf("0 object found!")
+		}
+		if result.ModifiedCount < 1 {
+			t.Errorf("O object modified!")
 		}
 	}
 }
